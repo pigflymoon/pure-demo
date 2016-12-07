5314bf580618203a52f037919e5f503dcc119ac8
pigflymoon <pplflymoon2015@gmail.com>
2016-12-07 17:24:47 +1300

    # add routes # add pure css framework

--- server.babel.js
+++ server.babel.js
@@ -1,5 +1,28 @@
 import express from 'express';
+import path from 'path';
+import favicon from 'serve-favicon';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import hb from 'hbs';
+
+import index from './routes/index';
+import grid from './routes/grid';
+
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.engine('html', hb.__express);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/grid', grid);
+
+module.exports = app;
 
 app.use('/', express.static('public'));
 app.listen(process.env.PORT || 3000);
